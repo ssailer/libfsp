15f8be04f2b38d165c80350868035f3ac09ab579
Simon Sailer <simon.sailer@mpi-hd.mpg.de>
2024-05-02 14:30:13 +0200

    Added docstrings.

--- src/fsp_state.h
+++ src/fsp_state.h
@@ -42,47 +42,49 @@
 
 typedef union STFlags {
   struct {
-    uint8_t hwm_multiplicity;
-    uint8_t hwm_prescaled;
-    uint8_t wps_abs;
-    uint8_t wps_rel;
-    uint8_t wps_prescaled;
-    uint8_t ct_multiplicity;
+    uint8_t hwm_multiplicity; // the multiplicity threshold has been reached
+    uint8_t hwm_prescaled; // the event was prescaled due to the HWM condition
+    uint8_t wps_abs; // the absolute peak sum threshold was reached
+    uint8_t wps_rel; // the relative peak sum threshold was reached and a coincidence to a reference event is fulfilled
+    uint8_t wps_prescaled; // the event was prescaled due to the WPS condition
+    uint8_t ct_multiplicity; // a channel was above the ChannelThreshold condition
   };
   uint64_t is_flagged;
 } STFlags;
 
 typedef union EventFlags {
   struct {
-    uint8_t is_retrigger;
-    uint8_t is_extended;
+    uint8_t is_retrigger; // the event is a retrigger event
+    uint8_t is_extended; // the event triggered (a) retrigger event(s)
   };
   uint64_t is_flagged;
 } EventFlags;
 
+// Windowed Peak Sum
 typedef union WPSFlags {
   struct {
-    uint8_t abs_threshold;
-    uint8_t rel_threshold;
-    uint8_t rel_reference;
-    uint8_t rel_pre_window;
-    uint8_t rel_post_window;
+    uint8_t abs_threshold; // absolute threshold was reached
+    uint8_t rel_threshold; // relative threshold was reached
+    uint8_t rel_reference; // the event is a WPS reference event
+    uint8_t rel_pre_window; // the event is in the pre window of a reference event
+    uint8_t rel_post_window; // the event is in the post window of a reference event
   };
   uint64_t is_flagged;
 } WPSFlags;
 
+// Hardware Multiplicity
 typedef union HWMFlags {
   struct {
-    uint8_t multiplicity_threshold;
-    uint8_t multiplicity_below;
+    uint8_t multiplicity_threshold; // the multiplicity threshold (number of channels) has been reached
+    uint8_t multiplicity_below; // all non-zero channels have an hardware value below the set amplitude threshold
   };
   uint64_t is_flagged;
 } HWMFlags;
 
+// Channel Threshold
 typedef union CTFlags {
   struct {
-    uint8_t multiplicity;
-    // uint8_t is_extended;
+    uint8_t multiplicity; // number of triggered channels (amplitude above threshold)
   };
   uint64_t is_flagged;
 } CTFlags;
@@ -95,13 +97,11 @@ typedef struct FSPFlags {
   WPSFlags wps;
   HWMFlags hwm;
   CTFlags ct;
-  // unsigned int trigger;
-  // unsigned int event;
 
 } FSPFlags;
 
-// Windows Peak Sum
 typedef struct FSPObervables {
+  // Windows Peak Sum
   struct wps_obs {
     float max_value; // what is the maximum PE within the integration windows
     int max_offset;  // when is the total sum offset reached?
@@ -124,15 +124,15 @@ typedef struct FSPObervables {
 
   // Channel Threshold 
   struct ct_obs {
-    int multiplicity;
-    unsigned short max[FCIOMaxChannels];
-    int trace_idx[FCIOMaxChannels];
-    const char* label[FCIOMaxChannels];
+    int multiplicity; // how many channels were above the threshold
+    unsigned short max[FCIOMaxChannels]; // the maximum per channel
+    int trace_idx[FCIOMaxChannels]; // the corresponding fcio trace index
+    const char* label[FCIOMaxChannels]; // the name of the channel given during setup
 
   } ct;
 
   struct event_obs {
-    int nextension;
+    int nextension; // if we found re-triggers how many events are consecutive from then on. the event with the extension flag carries the total number
   } evt;
 
 } FSPObservables;
